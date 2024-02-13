6ffd808c1abe608e8e3d7f170a05b6954da672e1
AY-ID <159939650+AY-ID@users.noreply.github.com>
2024-02-13 20:14:04 +0000

    Add files via upload

--- first.py
+++ first.py
@@ -1,11 +1,29 @@
-name="James"
-firstname='Luke'
+#name="James"
+#firstname='Luke'
 
-age=40
+#age=40
 # firstName= camel notation hbghjbgjhhghg
-last_name="Kyron"
-adult=True
-Sex='Male'
+#last_name="Kyron"
+#adult=True
+#Sex='Male'
 
 #Data types integer string, boolean,decimal
-print(firstname,age,adult,Sex)
+#print(firstname,age,adult,Sex)
+#if 56 > 21:
+  ##print('yes')
+x = "awesome"
+def myfunc():
+    x = "fantastic"
+    def myfunc():
+        x = "sweet"
+        def myfunc():
+            x = 'Python'    
+            print('I love '+ x)
+        
+        myfunc()
+        print('Python is '+ x)    
+    myfunc()
+    print('Python is '+ x)
+        
+myfunc()
+print('Python is '+ x)
