a8aeac2ab97faa26722ad7547e4b0579c7864ccd
AY-ID <159939650+AY-ID@users.noreply.github.com>
2024-02-13 17:16:26 +0000

    Add files via upload

---
+++ first.py
@@ -0,0 +1,11 @@
+name="James"
+firstname='Luke'
+
+age=40
+# firstName= camel notation hbghjbgjhhghg
+last_name="Kyron"
+adult=True
+Sex='Male'
+
+#Data types integer string, boolean,decimal
+print(firstname,age,adult,Sex)
